85d32251592b919c4002ffb6fa43ad5fd0b602a8
Quirijn Slings <quirijn.slings@trivident.com>
2018-11-20 13:17:53 +0100

    Upgraded to 2.3.0.1 (release)

--- source/DD4T.Providers.Tridion9.CIL/Properties/AssemblyInfo.cs
+++ source/DD4T.Providers.Tridion9.CIL/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.3.0.0")]
 [assembly: AssemblyFileVersion("2.3.0.0")]
-[assembly: AssemblyInformationalVersion("2.3.0.0-alpha1")]
+[assembly: AssemblyInformationalVersion("2.3.0.1")]
